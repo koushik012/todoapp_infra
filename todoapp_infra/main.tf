d601a3f32d3c3c89f415243e881f667f3c2783bc
DevOps Insiders <124057238+devopsinsiders@users.noreply.github.com>
2025-06-29 11:47:29 +0530

    JIRA -101 Add new RG and Dhoom 3 Story (#3)
    * added dhhom3 stry
    
    * added new rg
    
    * updated code

--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -16,6 +16,13 @@ module "resource_group" {
   resource_group_location = "centralindia"
 }
 
+# This RG is created as part of JIRA-101 Ticket for Project TodoApp
+module "resource_group1" {
+  source                  = "../modules/azurerm_resource_group"
+  resource_group_name     = "rg-todoapp1"
+  resource_group_location = "centralindia"
+}
+
 module "virtual_network" {
   depends_on = [module.resource_group]
   source     = "../modules/azurerm_virtual_network"
